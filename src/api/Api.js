e4654fdba2a416df3be15e13b7a49c5f3ec34e2c
mmxfxqr <holodov.sas@yandex.ru>
2024-06-11 14:14:43 +0300

    new adress for api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5125/api";
+export const API_URL = "https://a27684-abed.t.d-f.pw/api";
 const $api = axios.create({
   baseURL: API_URL,
 });
